90aff3b46bc17f8a9c1d6b7bae57380ef2df30e4
Igor Coelho da Costa <74683510+whothekid@users.noreply.github.com>
2024-10-13 03:48:38 +0000

    Complementos no header.

--- flashcard-companion/src/app/app.module.ts
+++ flashcard-companion/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+
 
 
 @NgModule({
@@ -21,7 +24,9 @@ import { MatButtonModule } from '@angular/material/button';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
